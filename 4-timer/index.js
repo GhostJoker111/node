b267be49d3a629d90998439ca751fdcd1246c9ba
Artem Zhurkin <jurkin@axlebolt.com>
2025-01-26 17:30:03 +0400

    4-timer corrections

--- 4-timer/index.js
+++ 4-timer/index.js
@@ -2,9 +2,12 @@ const EventEmitter = require("events")
 
 const timerEmitter = new EventEmitter();
 
-let firstArg = process.argv[2] ?? ""
-let secondArg = process.argv[3] ?? ""
-let thirdArg = process.argv[4] ?? ""
+const checkArgs = (...args) => {
+    if (args.every(arg => arg === undefined || arg === "")) {
+        console.log("Введите хотя бы один аргумент!");
+        process.exit(1);
+    }
+}
 
 const timeForTimer = (...args) => {
     let totalMs = 0;
@@ -12,25 +15,32 @@ const timeForTimer = (...args) => {
     for (const arg of args) {
         if (!arg) continue;
 
-        if (arg.includes("h")) {
-            const hours = Number(arg.replace("h", ""));
-            totalMs += hours * 3600000;
-        }
-        else if (arg.includes("min")) {
-            const minutes = Number(arg.replace("min", ""));
-            totalMs += minutes * 60000;
-        }
-        else if (arg.includes("sec")) {
-            const seconds = Number(arg.replace("sec", ""));
-            totalMs += seconds * 1000;
+        const timeType = arg.slice(-1)
+        const timeValue = arg.slice(0, -1)
+
+        switch (timeType) {
+            case 'h':
+                totalMs += timeValue * 3600000;
+                break;
+            case 'm':
+                totalMs += timeValue * 60000;
+                break;
+            case 's':
+                totalMs += timeValue * 1000;
+                break;
+            default:
+                console.log(`Неизвестный формат времени: ${timeType}. Введите корректный формат(пример: 1h 30m 45s`);
+                process.exit(1);
         }
     }
 
     return totalMs;
 };
 
+checkArgs(process.argv[2], process.argv[3], process.argv[4])
+
 timerEmitter.on("timer", (time) => setTimeout(() => {
     console.log("Wake up, Samurai!")
 }, timeForTimer(time)))
 
-timerEmitter.emit("timer", firstArg, secondArg, thirdArg)
+timerEmitter.emit("timer", process.argv[2], process.argv[3], process.argv[4])
